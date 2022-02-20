4952963d198b186508f4e5ff221da1fbd60a1db8
Leonardo Martes <>
2022-02-20 18:44:20 -0500

    Fonts and controller view identifiers are added in the app constants.

--- common-utils/app-constants/AppConstants.swift
+++ common-utils/app-constants/AppConstants.swift
@@ -1,3 +1,26 @@
 import Foundation
 
 let API_POST_LIST = "https://jsonplaceholder.typicode.com/posts"
+
+enum titles: String {
+    case allPost = "All"
+    case favoritesPost = "Favorites"
+}
+
+struct LatoFonts {
+    static let black = "Lato-Black"
+    static let blackItalic = "Lato-BlackItalic"
+    static let bold = "Lato-Bold"
+    static let boldItalic = "Lato-Italic"
+    static let italic = "Lato-Italic"
+    static let light = "Lato-Light"
+    static let lightItalic = "Lato-LightItalic"
+    static let regular = "Lato-Regular"
+    static let thin = "Lato-Thin"
+    static let thinItalic = "Lato-ThinItalic"
+}
+
+struct Identifier {
+    static let postsViewController = "PostsViewControllerIdentifier"
+    static let favoritesViewController = "FavoritesViewControllerIdentifier"
+}
