bae43e17eac83bb9e081f4c2d81ec11a707903c0
Leonardo Martes <>
2022-02-20 17:32:44 -0500

    The request or all post is made.

--- zemoga-ios-test/app-modules/list-post-module/Interactor/ListPostInteractor.swift
+++ zemoga-ios-test/app-modules/list-post-module/Interactor/ListPostInteractor.swift
@@ -1,10 +1,26 @@
 import Foundation
+import Alamofire
+import ObjectMapper
 
-class ListPostInteractor: PresenterToInteractorListPostProtocol {
-    var presenter: InteractorToPresenterListPostProtocol?
+class ListPostInteractor: PresenterToInteractorPostListProtocol {
+    var presenter: InteractorToPresenterPostListProtocol?
     
     func fetchPostList() {
         print("Get list post...")
+        AF.request(API_POST_LIST).response { (response) in
+            self.handleResponse(requestResponse: response)
+        }
+    }
+    
+    private func handleResponse(requestResponse: AFDataResponse<Data?>) {
+        if let error = requestResponse.error {
+            presenter?.listPostFetchFailed(with: error)
+            return
+        }
+        
+        if let data = requestResponse.data {
+            print("data: ", data)
+        }
     }
     
 }
